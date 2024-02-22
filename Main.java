72f4a3ab92eddef036117fe04abc27459fc8c986
Liza <ye.haponenko@ukma.edu.ua>
2024-02-22 20:05:57 +0200

    add/edit/delete faculty

--- Main.java
+++ Main.java
@@ -1,20 +1,60 @@
 //клас-виконавець
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int maxSize = DataInput.getInt("Скільки факультетів матиме університет? ");
+    public static void main(String[] args) throws IOException {
+        int maxSize = DataInput.getInt("Скiльки факультетiв матиме унiверситет?");
         University university = new University(maxSize);
+        
+        System.out.println("1. ~Додати факультет~ ");
+        System.out.println("2. ~Редагувати факультет~ ");
+        System.out.println("3. ~Видалити факультет~ ");
+        System.out.println("4. ~Вихiд~");
 
-        // Додавання факультетів
-        university.createFaculty("Інформатики");
-        university.createFaculty("Міжнародних відносин");
-        university.createFaculty("Філології");
+        int choose = DataInput.getInt("Оберiть дiю: ");
 
-        // Показати початковий список факультетів
-        university.Faculties();
+        while (choose < 4) {
+
+        switch (choose) {
+        case 1:
+
+            int count = DataInput.getInt("Скiльки хочете додати зараз?");
+
+            for (int i = 0; i < count; i++){
+            String nameOfFaculty = DataInput.getString("Введiть назву факультету");
+            university.createFaculty(nameOfFaculty);
+            }
+            university.Faculties();
+            
+        break;
+        
+        case 2:
+            university.Faculties();
+            String previousName = DataInput.getString("Попередня  назва факультету: ");
+            String newName = DataInput.getString("Нова назва факультету: ");
+            university.editFaculty(previousName,newName);
+            university.Faculties();
+
+        break;
+
+        case 3:
+            university.Faculties();
+            String nameOfFaculty = DataInput.getString("Назва факультету, який хочете видалити");
+            university.deleteFaculty(nameOfFaculty);
+            university.Faculties();
+
+        break;
+
+        default:
+
+        break;
+        }
+        choose = DataInput.getInt("Оберiть дiю: ");
 
     }
+}
 
     
 }
--- University.java
+++ University.java
@@ -11,9 +11,12 @@ public class University {
 
     /**метод виводу всіх факультетів університету */
     public void Faculties() {
-        System.out.println("Список факультетів:");
+        System.out.println("Список факультетiв:");
         for (int i = 0; i < size; i++) {
             System.out.println(faculties[i].getNameOfFaculty());
+        }
+        if (size == 0){
+            System.out.println("Наразi ви не додали жоден факультет");
         }
     }
 
@@ -23,18 +26,44 @@ public class University {
             Faculty faculty = new Faculty(nameOfFaculty);
             faculties[size] = faculty;
             size++;
-            System.out.println("Факультет " + nameOfFaculty + " успішно створений.");
+            System.out.println("Факультет " + nameOfFaculty + " успiшно створений");
         } else {
-            System.out.println("Неможливо додати новий факультет. Максимальна кількість факультетів досягнута.");
+            System.out.println("Неможливо додати новий факультет. Максимальна кiлькiсть факультетiв досягнута");
         }
     }
 
-    public void deleteFaculty(){
-        
+    public void deleteFaculty(String nameOfFaculty) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            if (faculties[i].getNameOfFaculty().equals(nameOfFaculty)) {
+                for (int j = i; j < size - 1; j++) {
+                    faculties[j] = faculties[j + 1];
+                }
+                size--;
+                found = true;
+                System.out.println("Факультет " + nameOfFaculty + " успiшно видалений");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Факультет з назвою " + nameOfFaculty + " не знайдено");
+        }
     }
 
-    public void editFaculty(){
-        
+    public void editFaculty(String previousName, String newName) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            if (faculties[i].getNameOfFaculty().equals(previousName)) {
+                faculties[i].setNameOfFaculty(newName);
+                found = true;
+                System.out.println("Назву факультету успiшно змiнено з " + previousName + " на " + newName);
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Факультет з назвою " + previousName + " не знайдено");
+        }
     }
+    
 
 }
