5a6a4f28bf9e74464bff48ebfcc341f24feb1dac
Vika Romaniuk <vikaromaniuk345@gmail.com>
2024-02-24 14:59:48 +0200

    Changes have been made to Department and Students

--- Human.java
+++ Human.java
@@ -8,18 +8,19 @@ class Human {
     //вік людини
     private int age;
 
+
     //посада людини, по типу: студент, викладач
     private String position;
 
 
-
+//конструктор створення людини, в подальшому класи наслідуватимуть його
     public Human(String name, int age, String position) {
         this.name = name;
         this.age = age;
         this.position = position;
     }
 
-    // Getters and setters
+    // геттери і сеттери
     public String getName() {
         return name;
     }
@@ -36,51 +37,83 @@ class Human {
         this.age = age;
     }
 
-    @Override
-    public String toString() {
-        return "Name: " + name + ", Age: " + age;
+    public String getPosition() {
+        return position;
     }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
 }
 
 class Student extends Human {
-    private String[] grades;
+    private int grades;
 
-    public Student(String name, int age, String position, String[] grades) {
+
+    private int course;
+
+    public Student(String name, int age, String position, int grades, int course) {
         super(name, age, position);
         this.grades = grades;
+        this.course = course;
+    }
+
+    //геттери і сеттери для курсу студента
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
     }
 
     // геттери і сеттери для оцінок
-    public String[] getGrades() {
+    public int getGrades() {
         return grades;
     }
 
-    public void setGrades(String[] grades) {
+    public void setGrades(int grades) {
         this.grades = grades;
     }
 
+    //виведення інформації про об'єкт "Student"
+
     @Override
     public String toString() {
-        return "Студент: " + getName() + "\n" +
-                "Середня оцінка: " + Arrays.toString(grades) + "\n" + ""  ;
+        return "Позиція: " + getPosition() + "\n" +
+                "Ім'я: " + getName() + "\n" +
+                "Курс: " + getCourse() + "\n" +
+                "Вік студента: " + getAge() + "\n" +
+                "Середня оцінка: " + getGrades();
     }
 }
 
-class Teacher extends Human {
+
+
+class Lecturer extends Human {
     private String department;
 
-    public Teacher(String name, int age, String position, String department) {
+    public Lecturer(String name, int age, String position, String department) {
         super(name, age, position);
         this.department = department;
     }
 
-    // Getter and setter for department
     public String getDepartment() {
         return department;
     }
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    //виведення інформації про об'єкт "Teacher"
+    @Override
+    public String toString() {
+        return  "Позиція: " + getPosition() + "\n" +
+                "Ім'я: " + getName() + "\n" +
+                "Вік викладача: " + getAge() + "\n" +
+                "Посада: " +  getDepartment();
     }
 
 }
